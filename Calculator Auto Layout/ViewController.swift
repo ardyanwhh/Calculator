f805e8090dd6622b5f5010ddb7473b743208e7bd
Ardyan Wahyu <ardyanwhh@icloud.com>
2020-01-22 13:33:35 +0700

    done

---
+++ Calculator Auto Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator Auto Layout
+//
+//  Created by Ardyan Wahyu on 22/01/20.
+//  Copyright © 2020 ardyanwhh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
